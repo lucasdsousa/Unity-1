e96311fbe1b40fa93a3684786de7322cae548183
Lucas Santana <lucas.dsousast@gmail.com>
2021-10-01 05:30:53 -0300

    Gun Rotation Fix and animation not working

--- Another 3D game for tests/Assets/GrapplingGun.cs
+++ Another 3D game for tests/Assets/GrapplingGun.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 
 public class GrapplingGun : MonoBehaviour {
 
-    private LineRenderer lr;
     private Vector3 grapplePoint;
     public LayerMask whatIsGrappleable;
     public Transform gunTip, camera, player;
     private float maxDistance = 100f;
     private SpringJoint joint;
-
-    void Awake() {
-        lr = GetComponent<LineRenderer>();
-    }
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
@@ -20,11 +15,6 @@ public class GrapplingGun : MonoBehaviour {
         else if (Input.GetMouseButtonUp(0)) {
             StopGrapple();
         }
-    }
-
-    //Called after Update
-    void LateUpdate() {
-        DrawRope();
     }
 
     /// <summary>
@@ -48,9 +38,6 @@ public class GrapplingGun : MonoBehaviour {
             joint.spring = 4.5f;
             joint.damper = 7f;
             joint.massScale = 4.5f;
-
-            lr.positionCount = 2;
-            currentGrapplePosition = gunTip.position;
         }
     }
 
@@ -59,20 +46,7 @@ public class GrapplingGun : MonoBehaviour {
     /// Call whenever we want to stop a grapple
     /// </summary>
     void StopGrapple() {
-        lr.positionCount = 0;
         Destroy(joint);
-    }
-
-    private Vector3 currentGrapplePosition;
-    
-    void DrawRope() {
-        //If not grappling, don't draw rope
-        if (!joint) return;
-
-        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
-        
-        lr.SetPosition(0, gunTip.position);
-        lr.SetPosition(1, currentGrapplePosition);
     }
 
     public bool IsGrappling() {
